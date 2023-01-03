7b5c64e294016b23e23c1e50bcc6287176ae286f
SALTU-19 <69312350+SALTU-19@users.noreply.github.com>
2023-01-03 23:38:41 +0300

    hw4 done

--- HW 4/part1.pl
+++ HW 4/part1.pl
@@ -1,17 +1,23 @@
+:- dynamic(course/7).
+:- dynamic(room/5).
+:- dynamic(instructor/6).
+:- dynamic(student/3).
+
+
 % rooms
 
 % id-capacity-equipment-availability
 room(room1, 10, whiteboard,8,12).
-room(room2, 20, projector,9,15).
+room(room2, 20, whiteboard,9,15).
 room(room3, 30, smartboard,12,17).
 
 % courses
 
-% id-instructor-capacity-availability-room
+% id-instructor-capacity-availability-equipment-room
 
-course(course1, instructor1, 10, 9,10).
-course(course2, instructor2, 20, 9,15).
-course(course3, instructor3, 30, 12,17).
+course(course1, instructor1, 10, 9,10,whiteboard,room1).
+course(course2, instructor2, 20, 9,15,projector,room2).
+course(course3, instructor3, 30, 12,17,smartboard,room3).
 
 % instructors
 
@@ -30,53 +36,89 @@ student(student2, [course2,course3], true).
 student(student3, [course3], false).
 
 
+% Check conflict between two courses
+% check_conflict(+Course1, +Course2)
+part_1(Course1, Course2) :-
+    course(Course1, _, _, Start1, End1, _, Room1),
+    course(Course2, _, _, Start2, End2, _, Room2),
+    Room1 == Room2,
+    Start1 =< Start2,
+    End1 >= Start2,
+    write('Course '), write(Course1), write(' and course '), write(Course2), write(' conflict'), nl.
 
 
 % Check which room can be assigned to a given class.
 % check_room_class(+Room, +Course)
-check_room_class(Room, Course) :-
-    room(Room, Capacity1, _, Start1, End1),
-    course(Course, _, Capacity2, Start2, End2),
+part_2(Room, Course) :-
+    room(Room, Capacity1, Equipment1, Start1, End1),
+    course(Course, _, Capacity2, Start2, End2,Equipment2,_),
     Start1 =< Start2,
     End1 >= End2,
     Capacity1 >= Capacity2,
+    Equipment1 == Equipment2,
     write('Room '), write(Room), write(' is available for course '), write(Course), nl.
 
 % Check which room can be assigned to which classes
 % check_rooms_class(+Course)
-check_rooms_class(Course) :-
-    course(Course, _, Capacity1, Start1, End1),
-    room(Room, Capacity2, _, Start2, End2),
-    Start2 =< Start1,
-    End2 >= End1,
-    Capacity2 >= Capacity1,
-    write('Room '), write(Room), write(' is available for course '), write(Course), nl.
+part_3(Course,Rooms) :-
+    course(Course, _, Capacity1, Start1, End1,Equipment1,_),
+    findall(Room, part_2(Room,Course), Rooms).
+
+
+
 
 % Check whether a student can be enrolled to a given class.
-% check_student_class(+Student, +Course)
-check_student_class(Student, Course) :-
-    student(Student, Courses, _),
-    member(Course, Courses),
-    write('Student '), write(Student), write(' can be enrolled to course '), write(Course), nl.
-
-% Check which room a student can be assigned.
 % check_students_class(+Student,+Room)
-check_students_class(Student,Room) :-
+part_4(Student,Room) :-
     student(Student, _, Handicapped),
     room(Room, _, Equipment, _, _),
     Handicapped == true,
     Equipment == projector,   
     write('Student '), write(Student), write(' can be assigned to room '), write(Room), nl.
-check_students_class(Student,Room) :-
+part_4(Student,Room) :-
     student(Student, _, Handicapped),
     room(Room, _, Equipment, _, _),
     Handicapped == true,
     Equipment == smartboard, 
     write('Student '), write(Student), write(' can be assigned to room '), write(Room), nl.
-check_students_class(Student,Room) :-
+part_4(Student,Room) :-
     student(Student, _, Handicapped),
     room(Room, _, _, _, _),
     Handicapped == false,
     write('Student '), write(Student), write(' can be assigned to room '), write(Room), nl.
+
+% Check which classes a student can be assigned.
+% check_students_class(+Student)
+part_5(Student,Rooms) :-
+    student(Student, _, Handicapped),
+    findall(Room, part_4(Student,Room), Rooms).
+
+
+
+
+
+
+
+
+
+
+
+% add a course
+% add_course(+Course, +Instructor, +Capacity, +Start, +End, +Equipment, +Room)
+add_course(Course, Instructor, Capacity, Start, End, Equipment, Room) :-
+    assert(course(Course, Instructor, Capacity, Start, End, Equipment, Room)).
+
+% add a room
+% add_room(+Room, +Capacity, +Equipment, +Start, +End)
+add_room(Room, Capacity, Equipment, Start, End) :-
+    assert(room(Room, Capacity, Equipment, Start, End)).
+% add a student
+% add_student(+Student, +Courses, +Handicapped)
+add_student(Student, Courses, Handicapped) :-
+    assert(student(Student, Courses, Handicapped)).
+% add an instructor
+% add_instructor(+Instructor, +Courses, +Equipment)
+add_instructor(Instructor, Courses, Equipment) :-
+    assert(instructor(Instructor, Courses, Equipment)).
 
 
--- HW 4/part2.pl
+++ HW 4/part2.pl
@@ -5,37 +5,39 @@ schedule(erzincan, antalya, 3).
 schedule(antalya, diyarbakir, 4).
 schedule(antalya, izmir, 2).
 schedule(diyarbakir, ankara, 8).
+schedule(van, gaziantep, 3).
+schedule(rize, ankara, 5).
+schedule(corum,ankara, 2).
 schedule(izmir, ankara, 6).
 schedule(izmir, istanbul, 2).
 schedule(ankara, istanbul, 1).
 schedule(ankara, rize, 5).
 schedule(ankara, van, 4).
 schedule(istanbul, rize, 4).
-schedule(van, gaziantep, 3).
-schedule(rize, ankara, 5).
+schedule(yozgat,corum, 2).
 
 % Rules
 
 connection(X, Y, C) :- 
-    shortest(X, Y, C). 
+    shortest_way(X, Y, C). 
 
 connection(X, Y, C) :-
-    shortest(Y, X, C).
+    shortest_way(Y, X, C).
 %%%
-shortest(X, Y, C) :-     
-    path(X, Y, C),      
-    \+ (path(X, Y, C1),  
+shortest_way(X, Y, C) :-     
+    way(X, Y, C),      
+    \+ (way(X, Y, C1),  
     C1 < C).            
 
-path(X, Y, C) :- path(X, Y, C, []). 
+way(X, Y, C) :- way(X, Y, C, []). 
 
-path(X, Y, C, Visited) :-
-    \+ member(X, Visited),     
+way(X, Y, C, Travalled) :-
+    \+ member(X, Travalled),     
     schedule(X, Y, C).            
     
-% Next rule works recursively.
-path(X, Y, C, Visited) :-
-    \+ member(X, Visited),        
+
+way(X, Y, C, Travalled) :-
+    \+ member(X, Travalled),        
     schedule(X, Z, C1),               
-    path(Z, Y, C2, [X|Visited]),    
+    way(Z, Y, C2, [X|Travalled]),    
     C is C1 + C2.                  
